f108738928cf9e4a9c171c9b0b54872eada2c88b
CHAKlBDEV <chakibthestorm@gmail.com>
2018-05-18 15:48:57 +0100

    locks you out after 4 logins a day, had to do it, i have exams on sunday

--- app/src/main/java/com/shakib/icenine/Login.java
+++ app/src/main/java/com/shakib/icenine/Login.java
@@ -10,10 +10,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Login extends AppCompatActivity {
     SharedPreferences mSharedPref;
     SharedPreferences.Editor mPrefEditor;
     EditText pass;
+    Boolean mAllowLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,11 @@ public class Login extends AppCompatActivity {
         ImageView cat = (ImageView) findViewById(R.id.imageView);
         pass = (EditText) findViewById(R.id.pass);
 
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        String currentDate = formatter.format(date);
+
+
         cat.setOnClickListener(new ImageView.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -36,26 +45,60 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        if(mSharedPref.contains("DATE")){
+            int sessionsleft = mSharedPref.getInt("SESSIONSLEFT", 1);
+            String savedDate = mSharedPref.getString("DATE", null);
+            if(savedDate.equals(currentDate)){
+                mAllowLogin = sessionsleft > 0;
+            }else{
+                mPrefEditor.putString("DATE", currentDate);
+                mPrefEditor.putInt("SESSIONSLEFT", 4);
+                mPrefEditor.commit();
+                mAllowLogin = true;
+            }
+
+        } else {
+            mPrefEditor.putString("DATE", currentDate);
+            mPrefEditor.putInt("SESSIONSLEFT", 4);
+            mPrefEditor.commit();
+            mAllowLogin = true;
+        }
 
     }
 
     private void check(){
+        if(!mAllowLogin){
+            meow();
+            return;
+        }
         if(mSharedPref.contains("PASS")){
             if(pass.getText().toString().equals(mSharedPref.getString("PASS", null))){
+                login();
                 Intent mainAct = new Intent(this, MainActivity.class);
                 mainAct.putExtra("pin", 101099);
                 startActivity(mainAct);
                 finish();
             } else {
-                Toast.makeText(this, "Meeow!", Toast.LENGTH_SHORT).show();
+                meow();
             }
         } else {
             mPrefEditor.putString("PASS", pass.getText().toString());
             mPrefEditor.commit();
+            login();
             Intent mainAct = new Intent(this, MainActivity.class);
             mainAct.putExtra("pin", 101099);
             startActivity(mainAct);
         }
+    }
+
+    private void meow(){
+        Toast.makeText(this, "Meeow!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void login(){
+        int sessionsleft = mSharedPref.getInt("SESSIONSLEFT", 1);
+        mPrefEditor.putInt("SESSIONSLEFT", sessionsleft - 1);
+        mPrefEditor.commit();
     }
 
 
